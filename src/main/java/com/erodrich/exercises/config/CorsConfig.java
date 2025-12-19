198c2bdf9f36d97b9756c354caf653f4a9c2992a
erodrichtorres <erodrich@arsys.es>
2025-12-19 22:17:56 +0100

    exercises-infra: trying to deploy

--- src/main/java/com/erodrich/exercises/config/CorsConfig.java
+++ src/main/java/com/erodrich/exercises/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.erodrich.exercises.config;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 String[] origins = allowedOrigins.split(",");
                 
                 registry.addMapping("/**")
